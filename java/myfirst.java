52c3cb1ef5f1f889f4c84247c3a0b945cbcc56c7
Slonik <slonik0002@gmail.com>
2019-05-19 14:15:38 -0400

    My First Java Program

---
+++ java/myfirst.java
@@ -0,0 +1,14 @@
+public class myfirst
+{
+   public static void main(String [] args)
+   {
+        String str="Hello World of Coffee!";
+        System.out.println(str);
+ 
+   }
+
+
+
+}
+
+
---
+++ java/mysecond.java
@@ -0,0 +1,11 @@
+public class mysecond
+{
+
+   public static void main(String [] args)
+   {
+	String str="Hello world of Java";
+	System.out.println(str);
+
+   }
+
+}
