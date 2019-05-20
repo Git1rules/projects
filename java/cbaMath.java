6b7206caf670c4f34f5cfe83a6f00de9bb46f997
Slonik <slonik0002@gmail.com>
2019-05-20 15:09:33 -0400

    Constructor for initialization has been added

--- java/cbaMath.java
+++ java/cbaMath.java
@@ -1,13 +1,41 @@
 class cbaMath
 {
 
-      private static int FibNumbers[];
+      private  int FibNumbers[];
           
       public static void main(String[]Args)
       {
 
-              FibNumbers[0]=0;
-           
+		           
+              cbaMath a=new cbaMath();
+
+	int w =	a.getFibbNumber(0);
+        System.out.println(w);        
+        
+
+      }//end of main
+
+
+      public int getFibbNumber(int i)
+      {
+
+            return FibNumbers[i];
 
       }
-}
+
+      public cbaMath()
+      {
+
+      FibNumbers=new int[10];
+      FibNumbers[0]=0;
+      FibNumbers[1]=1;
+      FibNumbers[2]=1;
+      FibNumbers[3]=2;
+      FibNumbers[4]=3;
+      FibNumbers[5]=5;
+      FibNumbers[6]=8;
+      FibNumbers[7]=13;
+      }
+
+
+} 
